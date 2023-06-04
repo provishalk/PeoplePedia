f63aacef82bcc64bbdba509fb033a3c1477fb4c7
provishalk <provishalk@gmail.com>
2023-06-04 19:31:30 +0530

    Huskey installed

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardIndexPage = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button disabled>Disabled button</button>
+    </div>
+  );
+};
+
+export default DashboardIndexPage;
