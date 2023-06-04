e2495c2e0267e0b025edd118dfeb8ae4e34ece4c
provishalk <provishalk@gmail.com>
2023-06-04 19:16:22 +0530

    configured react testing tool

---
+++ __test__/app/dashboard/page.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import DashboardIndexPage from "@/app/dashboard/page";
+
+describe("Dashboard page", () => {
+  it("Should render properly", () => {
+    render(<DashboardIndexPage />);
+    const header = screen.getByRole("heading");
+    const headerText = "Hello World";
+    expect(header).toHaveTextContent(headerText);
+  });
+
+  it("Should have disabled  button",()=>{
+    render(<DashboardIndexPage />);
+    const button = screen.getByRole("button");
+    expect(button).toBeDisabled()
+  })
+});
