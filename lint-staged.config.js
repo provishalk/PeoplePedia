b33b0cfa44a75e6f30b90e6c31f237a0cb881b5c
provishalk <provishalk@gmail.com>
2023-06-04 19:44:55 +0530

    testing huskey

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // this will check Typescript files
-  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+  "**/*.(ts|tsx)": () => "npm run tsc -- --noEmit",
 
-  // This will lint and format TypeScript and                                             //JavaScript files
+  // This will lint and format TypeScript and JavaScript files
   "**/*.(ts|tsx|js)": (filenames) => [
-    `yarn eslint --fix ${filenames.join(" ")}`,
-    `yarn prettier --write ${filenames.join(" ")}`,
+    `npm run eslint -- --fix ${filenames.join(" ")}`,
+    `npm run prettier -- --write ${filenames.join(" ")}`,
   ],
 
   // this will Format MarkDown and JSON
   "**/*.(md|json)": (filenames) =>
-    `yarn prettier --write ${filenames.join(" ")}`,
+    `npm run prettier -- --write ${filenames.join(" ")}`,
 };
