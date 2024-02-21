f08d3274cf51e4a7601d65494b5e0effb2599c6d
provishalk <72138651+provishalk@users.noreply.github.com>
2024-02-21 14:48:56 +0530

    Update page.tsx
    added script

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Script from 'next/script';
 
 const HomePage = () => {
   return (
     <div>
       <header className="bg-white py-8">
+        <Script
+        src="https://cdn.convrrt.com/libs/popup/widget.js"
+        strategy="beforeInteractive"
+      >
+        {`
+          (function (w, d, s, o, f, js, fjs) {
+            w[o] = w[o] || function () {
+              (w[o].q = w[o].q || []).push(arguments);
+            };
+            (js = d.createElement(s)),
+              (fjs = d.getElementsByTagName(s)[0]);
+            js.id = o;
+            js.src = f;
+            js.async = 1;
+            fjs.parentNode.insertBefore(js, fjs);
+          })(
+            window,
+            document,
+            "script",
+            "_cvt",
+            "https://cdn.convrrt.com/libs/popup/widget.js"
+          );
+          _cvt("init", {
+            orgID: 'mailmodo',
+            projectID: "0004ede9-b0b1-55d5-b0b2-819243f1c0ea_f5ad2069-61cf-4503-8b70-7e7b6eae5812",
+            popupID: "ec25ac2c-7a17-45be-94e8-13f093af2e6e"
+          });
+        `}
+      </Script>
         <div className="container mx-auto">
           <h1 className="text-4xl font-bold text-gray-800 text-center">
             Welcome to PeoplePedia
